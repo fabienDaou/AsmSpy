e2d18931f98eb68a567ff751623f0cc6bff66644
Fabien Daoulas <fabien.daoulas@gmail.com>
2018-02-22 19:42:36 +0100

    Create the MSTask project .

---
+++ AsmSpy.MSTask/DetectDllConflicts.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+using System;
+
+namespace AsmSpy.MSTask
+{
+    public class DetectDllConflicts : ITask
+    {
+        public IBuildEngine BuildEngine { get; set; }
+        public ITaskHost HostObject { get; set; }
+
+        public bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

84c0e783cd175e38064eb82c474794ee6556a9e5
Fabien Daoulas <fabien.daoulas@gmail.com>
2018-02-26 15:31:44 +0100

    Remove comment.

--- AsmSpy.MSTask/DetectNotFoundAssembliesTask.cs
+++ AsmSpy.MSTask/DetectNotFoundAssembliesTask.cs
@@ -104,15 +104,5 @@ namespace AsmSpy.MSTask
         {
             return toCompare.ToUpperInvariant().StartsWith(comparedTo, StringComparison.OrdinalIgnoreCase);
         }
-
-        //var directory = commandLineApplication.Argument("directory", "The directory to search for assemblies");
-        //var dgmlExport = commandLineApplication.Option("-dg|--dgml <filename>", "Export to a dgml file", CommandOptionType.SingleValue);
-        //var nonsystem = commandLineApplication.Option("-n|--nonsystem", "Ignore 'System' assemblies", CommandOptionType.NoValue);
-        //var all = commandLineApplication.Option("-a|--all", "List all assemblies and references.", CommandOptionType.NoValue);
-        //var noconsole = commandLineApplication.Option("-nc|--noconsole", "Do not show references on console.", CommandOptionType.NoValue);
-        //var silent = commandLineApplication.Option("-s|--silent", "Do not show any message, only warnings and errors will be shown.", CommandOptionType.NoValue);
-        //var bindingRedirect = commandLineApplication.Option("-b|--bindingredirect", "Create binding-redirects", CommandOptionType.NoValue);
-        //var referencedStartsWith = commandLineApplication.Option("-rsw|--referencedstartswith", "Referenced Assembly should start with <string>. Will only analyze assemblies if their referenced assemblies starts with the given value.", CommandOptionType.SingleValue);
-        //var includeSubDirectories = commandLineApplication.Option("-i|--includesub", "Include subdirectories in search", CommandOptionType.NoValue);
     }
 }
